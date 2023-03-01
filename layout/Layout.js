be8fd082d69e6a00b362b09cdbdc32a4fc1c95b7
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-03-01 19:57:55 -0300

    Instalar React Modal

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,7 +1,24 @@
 import Head from "next/head";
 import Sidebar from "@/components/Sidebar";
+import Modal from "react-modal";
+import useQuiosco from "@/hooks/useQuiosco";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#__next");
 
 export default function Layout({ children, pagina }) {
+  const { modal } = useQuiosco();
+
   return (
     <>
       <Head>
@@ -18,6 +35,12 @@ export default function Layout({ children, pagina }) {
           <div className="p-10 mt-10">{children}</div>
         </main>
       </div>
+
+      {modal && (
+        <Modal isOpen={modal} style={customStyles}>
+          <h1>Modal...</h1>
+        </Modal>
+      )}
     </>
   );
 }
