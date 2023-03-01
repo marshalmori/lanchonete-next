efc4ac2f6b129eb63236fc8a305c9cdfc1f787d2
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-02-28 23:04:18 -0300

    Mostrar a categoria atual na página principal

--- layout/Layout.js
+++ layout/Layout.js
@@ -5,7 +5,7 @@ export default function Layout({ children, pagina }) {
   return (
     <>
       <Head>
-        <title>Café - {pagina}</title>
+        <title>Coffee - {pagina}</title>
         <meta name="description" content="Quosco Cafetería" />
       </Head>
 
@@ -14,7 +14,7 @@ export default function Layout({ children, pagina }) {
           <Sidebar />
         </aside>
         <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
-          {children}
+          <div className="p-10 mt-10">{children}</div>
         </main>
       </div>
     </>
