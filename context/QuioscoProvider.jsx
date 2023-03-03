138dbe02b68c4ab5839bc6d4daf60db6525986c7
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-03-03 20:50:07 -0300

    Criar a lógica do botão editar para alterar as quantidades dos produtos

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -53,6 +53,12 @@ const QuioscoProvider = ({ children }) => {
     setModal(false);
   };
 
+  const handleEditarCantidades = (id) => {
+    const productoActualizar = pedido.filter((producto) => producto.id === id);
+    setProducto(productoActualizar[0]);
+    setModal(!modal);
+  };
+
   return (
     <QuioscoContext.Provider
       value={{
@@ -65,6 +71,7 @@ const QuioscoProvider = ({ children }) => {
         handleChangeModal,
         handleAgregarPedido,
         pedido,
+        handleEditarCantidades,
       }}
     >
       {children}
