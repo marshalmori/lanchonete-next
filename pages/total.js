9aabfa993968aed60266a181aa3bdef6d293254b
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-03-03 22:11:32 -0300

    Inserir os dados na página Total

--- pages/total.js
+++ pages/total.js
@@ -5,6 +5,34 @@ export default function Total() {
     <Layout pagina="Total">
       <h1 className="text-4xl font-black">Total</h1>
       <p className="text-2xl my-10">Confirma tu Pedido a Continuación</p>
+
+      <form>
+        <div>
+          <label
+            htmlFor="nombre"
+            className="block uppercase text-slate-800 font-bold text-xl"
+          >
+            Nombre
+          </label>
+
+          <input
+            id="nombre"
+            type="text"
+            className="bg-gray-200 w-full lg:w-1/3 mt-3 p-2 rounded"
+          />
+        </div>
+        <div className="mt-10">
+          <p className="text-2xl">
+            Total a pagar: {""} <span className="font-bold">$200</span>
+          </p>
+        </div>
+        <div className="mt-5">
+          <input
+            className="bg-indigo-700 w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center"
+            value="Confirmar pedido"
+          />
+        </div>
+      </form>
     </Layout>
   );
 }
