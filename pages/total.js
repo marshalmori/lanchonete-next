e7cd854869550dd36e8efa31d40542aafe955f0f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-03-06 15:18:25 -0300

    Calcular total a pagar

--- pages/total.js
+++ pages/total.js
@@ -1,9 +1,10 @@
 import { useEffect, useCallback } from "react";
 import Layout from "@/layout/Layout";
 import useQuiosco from "@/hooks/useQuiosco";
+import { formatearDinero } from "@/helpers";
 
 export default function Total() {
-  const { pedido, nombre, setNombre } = useQuiosco();
+  const { pedido, nombre, setNombre, colocarOrden, total } = useQuiosco();
 
   const comprobarPedido = useCallback(() => {
     return pedido.length === 0 || nombre === "" || nombre.length < 3;
@@ -12,11 +13,6 @@ export default function Total() {
   useEffect(() => {
     comprobarPedido();
   }, [pedido, comprobarPedido]);
-
-  const colocarOrden = (e) => {
-    e.preventDefault();
-    console.log("Enviando ordem...");
-  };
 
   return (
     <Layout pagina="Total">
@@ -42,7 +38,8 @@ export default function Total() {
         </div>
         <div className="mt-10">
           <p className="text-2xl">
-            Total a pagar: {""} <span className="font-bold">$200</span>
+            Total a pagar: {""}{" "}
+            <span className="font-bold">{formatearDinero(total)}</span>
           </p>
         </div>
         <div className="mt-5">
