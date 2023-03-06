1be35702d9e338bcf4fd2567a0f4eb4fd55677dc
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-03-06 15:05:34 -0300

    Verificar se o usuário colocou o nome para enviar o pedido

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -11,6 +11,7 @@ const QuioscoProvider = ({ children }) => {
   const [producto, setProducto] = useState({});
   const [modal, setModal] = useState(false);
   const [pedido, setPedido] = useState([]);
+  const [nombre, setNombre] = useState("");
 
   const router = useRouter();
 
@@ -82,6 +83,8 @@ const QuioscoProvider = ({ children }) => {
         pedido,
         handleEditarCantidades,
         handleEliminarProducto,
+        nombre,
+        setNombre,
       }}
     >
       {children}
--- pages/total.js
+++ pages/total.js
@@ -3,11 +3,11 @@ import Layout from "@/layout/Layout";
 import useQuiosco from "@/hooks/useQuiosco";
 
 export default function Total() {
-  const { pedido } = useQuiosco();
+  const { pedido, nombre, setNombre } = useQuiosco();
 
   const comprobarPedido = useCallback(() => {
-    return pedido.length === 0;
-  }, [pedido]);
+    return pedido.length === 0 || nombre === "" || nombre.length < 3;
+  }, [pedido, nombre]);
 
   useEffect(() => {
     comprobarPedido();
@@ -36,6 +36,8 @@ export default function Total() {
             id="nombre"
             type="text"
             className="bg-gray-200 w-full lg:w-1/3 mt-3 p-2 rounded"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
           />
         </div>
         <div className="mt-10">
